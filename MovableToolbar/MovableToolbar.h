92c3d33cf73cbed2cdadb3a9a416ac25e5b2b8ba
Kazuya Ueoka <k.ueoka@pieceofcake.co.jp>
2020-07-21 09:38:14 +0900

    Initial Commit

---
+++ MovableToolbar/MovableToolbar.h
@@ -0,0 +1,19 @@
+//
+//  MovableToolbar.h
+//  MovableToolbar
+//
+//  Created by Kazuya Ueoka on 2020/07/21.
+//  Copyright © 2020 fromkk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MovableToolbar.
+FOUNDATION_EXPORT double MovableToolbarVersionNumber;
+
+//! Project version string for MovableToolbar.
+FOUNDATION_EXPORT const unsigned char MovableToolbarVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MovableToolbar/PublicHeader.h>
+
+
